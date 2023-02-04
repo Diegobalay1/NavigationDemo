4c4fed302ac06138f841db9b51c89a390c2ffb21
balay <dlolhd@gmail.com>
2023-02-04 10:42:26 +0100

    Adding the profile screen.

---
+++ app/src/main/java/com/example/navigationdemo/screens/Profile.kt
@@ -0,0 +1,17 @@
+package com.example.navigationdemo.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Profile() {
+    Box(modifier = Modifier
+        .fillMaxSize(), 
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Profile Screen", style = MaterialTheme.typography.h5)
+    }
+}
